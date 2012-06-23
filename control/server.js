237d4c4cc79afe767643426729be4f890869264f
Ajay Tripathy <aurotripathy@auro-tripathys-MacBook-Pro.local>
2012-06-23 16:45:19 -0700

    mergerberger

--- control/server.js
+++ control/server.js
@@ -4,24 +4,35 @@ var app =  express.createServer();
 var mongo = require('mongoskin');
 var db = mongo.db('10.66.225.38:27017/angelhack?auto_reconnect');
 var Contacts = db.collection('contacts');
-
 // Initialize main server
 app.use(express.bodyParser());
 
 
 var mergeOrInsert = function (contactInfo) {
+  console.log(contactInfo.phones)
+  if (! (contactInfo.phones)){
+    contactInfo.phones = []
+  }
+  if (! (contactInfo.emails)){
+    contactInfo.emails = []
+  }
+  if (! (contactInfo.urls)){
+    contactInfo.urls = []
+  }
   var phoneQuery = {phones : {$in : contactInfo.phones} }
   var emailQuery = {emails : {$in: contactInfo.emails} } 
   var urlQuery = {urls: {$in: contactInfo.urls} }
-  var query = {$and : [ {userid : contactInfo.userid} , {$or : [ {phones : phoneQuery} , { emails : emailQuery } , { urls : urlQuery } ] } ] };
+  console.log(contactInfo.userid)
+  var query = {$and : [ {userid : contactInfo.userid} , {$or : [  phoneQuery , emailQuery , urlQuery  ] } ] };
+  //var query = {$and : [ {userid : contactInfo.userid} , phoneQuery  ] };
+  console.log(query)
   Contacts.findOne( query , function (err, result){
     if (err) {throw err; }
+    console.log(result)
     if (result){
       //update or merge the user
-      var phoneUpdate = { phones : { $each : contactInfo.phones } }
-      var emailUpdate = { emails : { $each : contactInfo.emails } }
-      var urlUpdate = { urls : { $each : contactInfo.urls } }
-      Contacts.updateById( result['_id'].toString() , { $addToSet : phoneUpdate , $addToSet : emailUpdate , $addToSet : urlUpdate } , function(err, result) {
+      var update = { phones : { $each : contactInfo.phones } , emails : { $each : contactInfo.emails }  ,  urls : { $each : contactInfo.urls }  } 
+      Contacts.updateById( result['_id'].toString() , { $addToSet : update   } , function(err, result) {
         if (err) {throw err;}
       })
     }
@@ -35,6 +46,7 @@ var mergeOrInsert = function (contactInfo) {
 
 app.post('/', function(req, res){
   var contactInfo;
+  console.log(req.body)
   if (Array.isArray(req.body)){
     for ( var i = 0 , ii = req.body.length ; i < ii ; i = i + 1){
       contactInfo = req.body[i];
